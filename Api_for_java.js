1aebd67df2b3fe7686e87950f8ebf2d5c3309517
EdwardMusumba <musumbaedward28@gmail.com>
2022-06-17 14:10:42 +0300

    commit12

---
+++ Api_for_java.js
@@ -0,0 +1,17 @@
+
+const endpoint = "http://localhost:3007/api/v1/signin";
+const homePageUrl = "../HTML/manage_newsitems.html";
+const editUrl = "../HTML/Editnewsitems.html";
+const searchUrl = "../HTML/search_newsitems.html";
+
+const endpoint = "http://localhost:3007/api/v1/edit_newsitem";
+const homePageUrl = "../HTML/edit_newsitems.html";
+const loginUrl="../LOGIN/login.html";
+
+const endpoint = "http://localhost:3007/api/v1/search_newsitems";
+const homePageUrl = "../HTML/search_newsitems.html";
+const loginUrl="../HTML/login.html";
+
+const endpoint = "http://localhost:3007/api/v1/register_newsitem";
+const homePageUrl = "../HTML/manage_newsitems.html";
+const loginUrl="../LOGIN/login.html";
